b587e6c354dd0e5afc9d1e43ffe9e0fcd6fae84b
lorelia1818 <laroca94@uan.edu.co>
2022-09-20 14:43:15 -0500

    confir

--- index.js
+++ index.js
@@ -1,38 +1,29 @@
-//Auxiliar Libraries
-const express = require('express');
+//auxiliar libreries
+const express = require ("express");
 
-//Database conection
-const {db_conection} = require('./database/connection');
+//database connection
 
-//Using express
+// using Express 
+
 const app = express();
 
 //Defining the API port
-app.set('port', process.env.PORT || 4000);
+app.set("port", process.env.PORT || 4000)
 
 //Middlewares
-app.use(express.json);
-
-//Limitations
-app.all('*', function(req, res, next){
-    res.header("Access-Con trol-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "*");
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-    next();
-});
-
-//Routes
-app.use('/api/users', require('./routes/user'));
-
-//Testing the db connection
-try {
-    const_db_status = db_conection.authenticate();
-    console.log("Connection has been established successfully.");
-} catch (error) {
-    console.error("Unable to connect to the database.", error);
+app.set (express.json())
+//limitations
+app.all("*" , function (req ,res ,next) {
+res.header("access-Control-allow-origin", "*");
+res.header("access-Control-allow-Headers", "*");
+res.header("access-Control-allow-Methods", "PUT , POST,GET,DELETE,OPTIONS ");
 }
+)
+// Routes
+//app.use ("/api/users", require ("./routes/user"));
 
-//Server
-app.listen(app.get('port'), () => {
-    console.log('Server in port: ', 4000);
-});
+//server
+app.listen(app.get("port") ,() => {
+
+    console.log("server in port:", 4000)
+})
