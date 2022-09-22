917d00fb8ef385bacd557f66ad2bd3b36046025a
lorelia1818 <laroca94@uan.edu.co>
2022-09-22 12:46:02 -0500

    confi

--- models/user.js
+++ models/user.js
@@ -1,59 +1,18 @@
 const{  DataTypes } = require('sequelize');
 const {db_connection} = require('./database/connection');
 
-const { user} = requiere ("./user")
-const {account} = requiere ("./account")
-const { auth} = requiere ("./auth")
-const { rol} = requiere ("./rol")
+const { User} = requiere ("./user")
+const {Account} = requiere ("./account")
+const { Auth} = requiere ("./auth")
+const { Rol} = requiere ("./rol")
 
-const user = db_connection.define("user",{
-    user_id {
-        tyoe: DataTypes.int
+const  account = db_connection.define ("accoun",{
+    account_id:{
+        type: DataTypes.int
     },
-    user_identificacion:{
-        type:DataTypes.Vachar(45)
-
-    },
-    user_name: {
-        Type: DataTypes.Vachar(100)
-    },
-    use_lastname: {
-        type: DataTypes.vachar(100)
+    account_idcity: {
+        type: DataTypes.int
 
     },
-    user_gmail: {
-        Type: DataTypes.Vachar(45)
-    },
-    use_phone: {
-        type: DataTypes.vachar(45)
-
-    },
-    use_pasword: {
-        type: DataTypes.vachar(45)
-
-    },
-    account_id: {
-        type:DataTypes.int,
-        references:{
-        model: account,
-        key:"id"
-        }
-    },
-    account_account_id: {
-        type:DataTypes.int,
-        references:{
-        model: account,
-        key:"id"
-        }
-    },
-    account_account_id: {
-        type:DataTypes.int,
-        references:{
-        model: account,
-        key:"id"
-        }
-    },
-    
-
 
 })
